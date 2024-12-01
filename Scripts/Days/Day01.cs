3c8bc73e2eda72a8654e002ecc204792d0953710
Nathan Istace <nathan.istace@gmail.com>
2024-12-01 18:15:35 +0100

    Day 1

---
+++ Scripts/Days/Day01.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Scripts.Days;
+
+public class Day01 : AbstractDay {
+	protected override int Day => 1;
+	private readonly Regex inputRegex = new(@"^(\d+) +(\d+)$");
+
+	public override string Part1() {
+		var input = GetInputLines().Select(t => inputRegex.Match(t)).Where(t => t.Success).Select(t => (int.Parse(t.Groups[1].Value), int.Parse(t.Groups[2].Value))).ToArray();
+		var left = input.Select(t => t.Item1).OrderBy(t => t).ToArray();
+		var right = input.Select(t => t.Item2).OrderBy(t => t).ToArray();
+		return $"{Enumerable.Range(0, left.Length).Select(i => Math.Abs(left[i] - right[i])).Sum()}";
+	}
+
+	public override string Part2() {
+		var input = GetInputLines().Select(t => inputRegex.Match(t)).Where(t => t.Success).Select(t => (int.Parse(t.Groups[1].Value), int.Parse(t.Groups[2].Value))).ToArray();
+		var left = input.Select(t => t.Item1).ToArray();
+		var right = input.Select(t => t.Item2).Distinct().ToDictionary(t => t, t => input.Count(u => t == u.Item2));
+		return $"{left.Sum(t => t * right.GetValueOrDefault(t, 0))}";
+	}
+}
---
+++ Scripts/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using AdventOfCode2024.Scripts.Days;
+
+namespace AdventOfCode2024.Scripts;
+
+public static class Program {
+	public static void Main() {
+		var day = new Day01();
+		Console.WriteLine($"{day.GetType().Name}:");
+		Stopwatch stopwatch = new();
+		stopwatch.Start();
+		Console.WriteLine($"PART 1:\t{day.Part1()}\n{stopwatch.ElapsedMilliseconds} ms");
+		stopwatch.Restart();
+		Console.WriteLine($"PART 2:\t{day.Part2()}\n{stopwatch.ElapsedMilliseconds} ms");
+		stopwatch.Stop();
+	}
+}
