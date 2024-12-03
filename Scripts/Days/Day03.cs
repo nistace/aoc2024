d335cfc7449a2d471706d3f08ad8555b61ec22a0
Nathan Istace <nathan.istace@gmail.com>
2024-12-03 08:42:42 +0100

    Day 03 Pt 1

---
+++ Scripts/Days/Day03.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Scripts.Days;
+
+public class Day03 : AbstractDay {
+	protected override int Day => 3;
+	private readonly Regex mulRegex = new(@"mul\((\d+),(\d+)\)");
+
+	public override string Part1() {
+		return $"{mulRegex.Matches(GetInputText()).Sum(t => int.Parse(t.Groups[1].Value) * int.Parse(t.Groups[2].Value))}";
+	}
+
+	public override string Part2() {
+		return "";
+	}
+}
--- Scripts/Program.cs
+++ Scripts/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2024.Scripts;
 
 public static class Program {
 	public static void Main() {
-		var day = new Day02();
+		var day = new Day03();
 		Console.WriteLine($"{day.GetType().Name}:");
 		Stopwatch stopwatch = new();
 		stopwatch.Start();
