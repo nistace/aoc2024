3d5aa90e3744235a427f005213fe8cab7664febd
nathan.istace <nathan.istace@fishingcactus.com>
2024-12-06 13:45:04 +0100

    Day 06 Pt 2 WIP

--- Scripts/Days/Day06.cs
+++ Scripts/Days/Day06.cs
@@ -1,25 +1,75 @@
 ﻿namespace AdventOfCode2024.Scripts.Days;
 
-public class Day06 : AbstractDay {
+public class Day06 : AbstractDay
+{
 	protected override int Day => 6;
 
-	public override string Part1() {
+	public override string Part1()
+	{
 		ParseInput(out var obstacles, out var startPosition, out var dimensions);
-		var visited = new HashSet<(int x, int y)>();
+		var simulated = Simulate(obstacles, startPosition, dimensions);
+
+		Debug(obstacles, startPosition, dimensions, simulated.visited);
+
+		return $"{Simulate(obstacles, startPosition, dimensions).visited.Count}";
+	}
+
+	public override string Part2()
+	{
+		ParseInput(out var obstacles, out var startPosition, out var dimensions);
+		var defaultVisited = Simulate(obstacles, startPosition, dimensions);
+
+		var possibleObstacles = defaultVisited.visited.SelectMany(t => t.Value.Select(u => u.MoveFrom(t.Key))).Distinct().Except(obstacles).Except([startPosition]);
+
+		return $"{possibleObstacles.Count(newObstacle => Simulate(obstacles.Union([newObstacle]).ToHashSet(), startPosition, dimensions).loop)}";
+	}
+
+	private static (bool loop, Dictionary<(int x, int y), HashSet<Heading>> visited) Simulate(HashSet<(int x, int y)> obstacles,
+		(int x, int y) startPosition,
+		(int width, int height) dimensions)
+	{
+		var visited = new Dictionary<(int x, int y), HashSet<Heading>>();
 		var heading = Heading.Up;
-		for (var currentPosition = startPosition; InGrid(currentPosition, dimensions); currentPosition = heading.MoveFrom(currentPosition)) {
-			visited.Add(currentPosition);
-			if (obstacles.Contains(heading.MoveFrom(currentPosition))) {
+		for (var currentPosition = startPosition; InGrid(currentPosition, dimensions); currentPosition = heading.MoveFrom(currentPosition))
+		{
+			if (!visited.ContainsKey(currentPosition)) visited.Add(currentPosition, []);
+			if (visited[currentPosition].Contains(heading))
+			{
+				return (true, visited);
+			}
+
+			visited[currentPosition].Add(heading);
+
+			if (obstacles.Contains(heading.MoveFrom(currentPosition)))
+			{
 				heading = heading.RotateRight();
 			}
 		}
 
-		return $"{visited.Count}";
+		return (false, visited);
 	}
 
-	public override string Part2() => "";
+	private static void Debug(HashSet<(int x, int y)> obstacles,
+		(int x, int y) startPosition,
+		(int width, int height) dimensions,
+		Dictionary<(int x, int y), HashSet<Heading>> visited)
+	{
+		for (var y = 0; y < dimensions.height; y++)
+		{
+			for (var x = 0; x < dimensions.width; x++)
+			{
+				if ((x, y) == startPosition) Console.Write("^");
+				else if (obstacles.Contains((x, y))) Console.Write("#");
+				else if (visited.ContainsKey((x, y))) Console.Write($"{visited[(x, y)].Count}");
+				else Console.Write(".");
+			}
+			Console.WriteLine();
+		}
 
-	private static bool InGrid((int x, int y) currentPosition, (int width, int height) dimensions) {
+	}
+
+	private static bool InGrid((int x, int y) currentPosition, (int width, int height) dimensions)
+	{
 		if (currentPosition.x < 0) return false;
 		if (currentPosition.y < 0) return false;
 		if (currentPosition.x >= dimensions.width) return false;
@@ -27,14 +77,17 @@ public class Day06 : AbstractDay {
 		return true;
 	}
 
-	private void ParseInput(out HashSet<(int, int)> obstacles, out (int, int) startPosition, out (int, int) dimensions) {
-		var allCharsCoords = GetInputLines().Select((chars, y) => (chars, y)).SelectMany(line => line.chars.Select((c, x) => (c, coords: (x, line.y)))).ToDictionary(t => t.coords, t => t.c);
+	private void ParseInput(out HashSet<(int, int)> obstacles, out (int, int) startPosition, out (int width, int height) dimensions)
+	{
+		var allCharsCoords = GetInputLines().Select((chars, y) => (chars, y)).SelectMany(line => line.chars.Select((c, x) => (c, coords: (x, line.y))))
+			.ToDictionary(t => t.coords, t => t.c);
 		obstacles = allCharsCoords.Where(t => t.Value == '#').Select(t => t.Key).ToHashSet();
 		startPosition = allCharsCoords.Single(t => t.Value == '^').Key;
 		dimensions = (allCharsCoords.Max(t => t.Key.x + 1), allCharsCoords.Max(t => t.Key.y + 1));
 	}
 
-	private readonly struct Heading : IEquatable<Heading> {
+	private readonly struct Heading : IEquatable<Heading>
+	{
 		public static Heading Up { get; } = new Heading(0, -1);
 		public static Heading Right { get; } = new Heading(1, 0);
 		public static Heading Down { get; } = new Heading(0, 1);
@@ -45,7 +98,8 @@ public class Day06 : AbstractDay {
 		private readonly int moveDeltaX;
 		private readonly int moveDeltaY;
 
-		private Heading(int moveDeltaX, int moveDeltaY) {
+		private Heading(int moveDeltaX, int moveDeltaY)
+		{
 			this.moveDeltaX = moveDeltaX;
 			this.moveDeltaY = moveDeltaY;
 		}
