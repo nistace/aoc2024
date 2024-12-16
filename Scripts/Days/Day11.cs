1c676c0397a1a3f260866f27357f7c93b5db17a8
Nathan Istace <nathan.istace@gmail.com>
2024-12-16 20:52:30 +0100

    Day 11 Pt2

--- Scripts/Days/Day11.cs
+++ Scripts/Days/Day11.cs
@@ -1,31 +1,42 @@
-﻿namespace AdventOfCode2024.Scripts.Days;
+﻿using System.Numerics;
+
+namespace AdventOfCode2024.Scripts.Days;
 
 public class Day11 : AbstractDay {
    protected override int Day => 11;
+   protected Dictionary<(BigInteger value, int blinks), long> CachedSplits { get; } = [];
+   protected Dictionary<BigInteger, BigInteger[]> CachedNextValues { get; } = [];
 
-   public override string Part1() {
-      ParseInput(out var initialValues);
-      var nextValues = new Dictionary<long, long[]> { { 0, [1] } };
+   public override string Part1() => $"{Simulate(25)}";
+   public override string Part2() => $"{Simulate(75)}";
 
-      return $"{initialValues.Sum(t => CountSubdivisions(t, 25, nextValues))}";
-   }
+   private long CountSubdivisions(BigInteger fromValue, int blinks) {
+      if (blinks == 0) return 1;
+      if (!CachedSplits.ContainsKey((fromValue, blinks))) {
+         if (!CachedNextValues.ContainsKey(fromValue)) {
+            var fromValueStr = $"{fromValue}";
+            if (fromValueStr == "0") CachedNextValues.Add(fromValue, [1]);
+            else if (fromValueStr.Length % 2 == 0) CachedNextValues.Add(fromValue, [long.Parse(fromValueStr[..(fromValueStr.Length / 2)]), long.Parse(fromValueStr[(fromValueStr.Length / 2)..])]);
+            else CachedNextValues.Add(fromValue, [fromValue * 2024]);
+         }
 
-   private static int CountSubdivisions(long fromValue, int overTimes, Dictionary<long, long[]> cachedNextValues) {
-      if (overTimes == 0) return 1;
-
-      if (!cachedNextValues.ContainsKey(fromValue)) {
-         var fromValueStr = "" + fromValue;
-         if (fromValueStr.Length % 2 == 0)
-            cachedNextValues.Add(fromValue, [long.Parse(fromValueStr[..(fromValueStr.Length / 2)]), long.Parse(fromValueStr[(fromValueStr.Length / 2)..])]);
-         else cachedNextValues.Add(fromValue, [fromValue * 2024]);
+         CachedSplits.Add((fromValue, blinks), CachedNextValues[fromValue].Sum(t => CountSubdivisions(t, blinks - 1)));
       }
 
-      return cachedNextValues[fromValue].Sum(t => CountSubdivisions(t, overTimes - 1, cachedNextValues));
+      return CachedSplits[(fromValue, blinks)];
+   }
+
+   private long Simulate(int blinks) {
+      ParseInput(out var initialValues);
+
+      for (var subBlinkCount = 1; subBlinkCount < blinks; ++subBlinkCount) {
+         initialValues.ForEach(t => CountSubdivisions(t, subBlinkCount));
+      }
+
+      return initialValues.Sum(t => CountSubdivisions(t, blinks));
    }
 
    private void ParseInput(out List<long> initialValues) {
       initialValues = GetInputText().Trim().Split(' ').Select(long.Parse).ToList();
    }
-
-   public override string Part2() => "";
 }
--- Scripts/Program.cs
+++ Scripts/Program.cs
@@ -5,7 +5,7 @@ namespace AdventOfCode2024.Scripts;
 
 public static class Program {
    public static void Main() {
-      var day = new Day16();
+      var day = new Day11();
       Console.WriteLine($"{day.GetType().Name}:");
       Stopwatch stopwatch = new();
       stopwatch.Start();
