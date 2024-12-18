d8b26e49b4b6e74fb0b815c4e05eb401b8115670
Nathan Istace <nathan.istace@gmail.com>
2024-12-19 00:55:40 +0100

    Day 14 Pt2

--- Scripts/Days/Day14.cs
+++ Scripts/Days/Day14.cs
@@ -13,8 +13,7 @@ public class Day14 : AbstractDay {
       var dimensions = new Vector2Int(101, 103);
       const int seconds = 100;
 
-      var robotFinalPositions = robotInfos.Select(t => t.Position + (t.Velocity * seconds))
-         .Select(t => new Vector2Int((t.X % dimensions.X + dimensions.X) % dimensions.X, (t.Y % dimensions.Y + dimensions.Y) % dimensions.Y)).ToArray();
+      var robotFinalPositions = SimulateRobotsMovement(robotInfos, seconds, dimensions);
 
       var countPerQuadrant = new[] {
          robotFinalPositions.Count(t => t.X < dimensions.X / 2f - 1 && t.Y < dimensions.Y / 2f - 1),
@@ -27,23 +26,36 @@ public class Day14 : AbstractDay {
    }
 
    public override string Part2() {
-      return "";
+      ParseInput(out var robotInfos);
+
+      var dimensions = new Vector2Int(101, 103);
+
+      var bestScore = -1;
+      var bestSecond = -1;
+      for (var second = 0; second < 20000; ++second) {
+         var secondScore = GetChristmasTreeScore(SimulateRobotsMovement(robotInfos, second, dimensions));
+         if (secondScore > bestScore) {
+            bestScore = secondScore;
+            bestSecond = second;
+         }
+      }
+
+      return $"{bestSecond}";
+   }
+
+   private static int GetChristmasTreeScore(IReadOnlyList<Vector2Int> robotFinalPositions) {
+      var distinctPositions = robotFinalPositions.Distinct().ToHashSet();
+      return distinctPositions.Sum(coordinates => coordinates.FourNeighbours.Count(t => distinctPositions.Contains(t)));
+   }
+
+   private IReadOnlyList<Vector2Int> SimulateRobotsMovement(IReadOnlyList<RobotInfo> robotInfos, int seconds, Vector2Int dimensions) {
+      return robotInfos.Select(t => t.Position + (t.Velocity * seconds))
+         .Select(t => new Vector2Int((t.X % dimensions.X + dimensions.X) % dimensions.X, (t.Y % dimensions.Y + dimensions.Y) % dimensions.Y)).ToArray();
    }
 
    private void ParseInput(out IReadOnlyList<RobotInfo> robotInfos) {
       robotInfos = GetInputLines().Select(t => inputLineRegex.Match(t)).Where(t => t.Success).Select(t =>
          new RobotInfo((int.Parse(t.Groups[1].Value), int.Parse(t.Groups[2].Value)), (int.Parse(t.Groups[3].Value), int.Parse(t.Groups[4].Value)))).ToArray();
-   }
-
-   // ReSharper disable once UnusedMember.Local
-   private static void Debug(IReadOnlyList<Vector2Int> robotFinalPositions, Vector2Int dimensions) {
-      for (var y = 0; y < dimensions.Y; y++) {
-         for (var x = 0; x < dimensions.X; x++) {
-            var robotCount = robotFinalPositions.Count(t => t.X == x && t.Y == y);
-            Console.Write(robotCount == 0 ? "." : robotCount > 10 ? "+" : $"{robotCount}");
-         }
-         Console.WriteLine();
-      }
    }
 
    private readonly struct RobotInfo(Vector2Int position, Vector2Int velocity) {
