8c2117f2dc171005a8ec61370bdb87f680a54a39
nathan.istace <nathan.istace@fishingcactus.com>
2024-12-20 13:06:53 +0100

    Day 20 Pt2

--- Scripts/Days/Day20.cs
+++ Scripts/Days/Day20.cs
@@ -5,24 +5,31 @@ namespace AdventOfCode2024.Scripts.Days;
 public class Day20 : AbstractDay {
    protected override int Day => 20;
 
-	public override string Part1() {
-		ParseInput(out var startPosition, out var endPosition, out var walls);
-		var paths = GetPaths(startPosition, walls);
-		var cheats = GetCheats(walls, paths);
+   public override string Part1() {
+      ParseInput(out var startPosition, out var walls);
+      return $"{GetCheats(GetPaths(startPosition, walls), 2, 100).Count}";
+   }
 
-		return $"{cheats.Count(t => t.Value >= 100)}";
-	}
-	
-	private static Dictionary<Vector2Int, int> GetCheats(HashSet<Vector2Int> walls, IReadOnlyDictionary<Vector2Int, int> costs) {
-		return (from cheatedWall in walls
-		        select (wall: cheatedWall, neighbouringPaths: cheatedWall.FourNeighbours.Where(costs.ContainsKey).ToArray())
-		        into cheat
-		        where cheat.neighbouringPaths.Length >= 2
-		        select (cheat.wall, savedCost: cheat.neighbouringPaths.Max(t => costs[t]) - cheat.neighbouringPaths.Min(t => costs[t]) - 2))
-			.ToDictionary(t => t.wall, t => t.savedCost);
-	}
+   public override string Part2() {
+      ParseInput(out var startPosition, out var walls);
+      return $"{GetCheats(GetPaths(startPosition, walls), 20, 100).Count}";
+   }
 
-   private IReadOnlyDictionary<Vector2Int, int> GetPaths(Vector2Int startPosition, HashSet<Vector2Int> walls) {
+   private static HashSet<(Vector2Int origin, Vector2Int destination)> GetCheats(IReadOnlyDictionary<Vector2Int, int> pathCosts, int allowedCheatDistance, int minCheatSavedCost) {
+      var cheats = new HashSet<(Vector2Int origin, Vector2Int destination)>();
+      foreach (var origin in pathCosts) {
+         foreach (var destination in pathCosts) {
+            if (origin.Value >= destination.Value) continue;
+            if (origin.Key.GridDistance(destination.Key) > allowedCheatDistance) continue;
+            if (destination.Value - origin.Value - origin.Key.GridDistance(destination.Key) < minCheatSavedCost) continue;
+
+            cheats.Add((origin.Key, destination.Key));
+         }
+      }
+      return cheats;
+   }
+
+   private static Dictionary<Vector2Int, int> GetPaths(Vector2Int startPosition, HashSet<Vector2Int> walls) {
       var closedCosts = new Dictionary<Vector2Int, int>();
       var openCosts = new Dictionary<Vector2Int, int> { { startPosition, 0 } };
 
@@ -39,15 +46,10 @@ public class Day20 : AbstractDay {
       return closedCosts;
    }
 
-   public override string Part2() {
-      return "";
-   }
-
-   public void ParseInput(out Vector2Int startPosition, out Vector2Int endPosition, out HashSet<Vector2Int> walls) {
+   public void ParseInput(out Vector2Int startPosition, out HashSet<Vector2Int> walls) {
       var charsAtCoords = GetInputLines().SelectMany((line, y) => line.Select((c, x) => (coordinates: new Vector2Int(x, y), c))).ToDictionary(t => t.coordinates, t => t.c);
 
       startPosition = charsAtCoords.Single(t => t.Value == 'S').Key;
-      endPosition = charsAtCoords.Single(t => t.Value == 'E').Key;
       walls = charsAtCoords.Where(t => t.Value == '#').Select(t => t.Key).ToHashSet();
    }
 }
