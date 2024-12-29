6af516d2ccdc6bc66180706a2538c94fee7247ee
Nathan Istace <nathan.istace@gmail.com>
2024-12-29 07:59:06 +0100

    Day 22 Pt 2 (not optimized)

--- Scripts/Days/Day22.cs
+++ Scripts/Days/Day22.cs
@@ -3,7 +3,7 @@
 public class Day22 : AbstractDay {
    protected override int Day => 22;
 
-   private const long Prune = 0xffffff;
+   private const int Prune = 0xffffff;
 
    public override string Part1() {
       ParseInput(out var numbers);
@@ -12,7 +12,39 @@ public class Day22 : AbstractDay {
    }
 
    public override string Part2() {
-      return "";
+      ParseInput(out var numbers);
+      var sellValuePerSequence = numbers.ToDictionary(t => t, GetSellValuesPerSequenceHash);
+      var distinctSequenceHashes = sellValuePerSequence.SelectMany(t => t.Value.Keys).Distinct();
+      var bestSellValue = distinctSequenceHashes.Select(sequenceHash => sellValuePerSequence.Values.Aggregate(0L, (current, sellValues) => current + sellValues.GetValueOrDefault(sequenceHash))).Max();
+      return $"{bestSellValue}";
+   }
+
+   public static Dictionary<int, long> GetSellValuesPerSequenceHash(long number) {
+      var sellValuesPerSequenceHash = new Dictionary<int, long>();
+      var sequenceDeltas = new Queue<int>();
+      var secret = number;
+      var previousPrice = (int)number % 10;
+      for (var i = 0; i < 2000; i++) {
+         secret = GetNextSecret(secret);
+         var price = (int)secret % 10;
+         sequenceDeltas.Enqueue(price - previousPrice);
+         if (sequenceDeltas.Count > 4) sequenceDeltas.Dequeue();
+         if (sequenceDeltas.Count == 4) {
+            var sequenceHash = GetSequenceHash(sequenceDeltas);
+            sellValuesPerSequenceHash.TryAdd(sequenceHash, price);
+         }
+         previousPrice = price;
+      }
+      return sellValuesPerSequenceHash;
+   }
+
+   private static int GetSequenceHash(IEnumerable<int> sequence) {
+      var hash = 0;
+      foreach (var value in sequence) {
+         hash <<= 5;
+         hash += value + 10;
+      }
+      return hash;
    }
 
    private static long GetNextSecret(long secret) {
